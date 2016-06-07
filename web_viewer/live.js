4c9bccba18cfef89099f644cb8b1874d97ef9e87
Menno van der Graaf <mennovandergraaf@hotmail.com>
2016-06-08 01:07:46 +0200

    add base implementation of AJAX functionatily

--- web_viewer/live.js
+++ web_viewer/live.js
@@ -1,38 +1,48 @@
 google.charts.load('current', {'packages':['line']});
-google.charts.setOnLoadCallback(get_data);
+google.charts.setOnLoadCallback(load_data);
 
-function get_data(){
-    var data = [[0,   4.2,  6.2,  3.4],
-    [1,  37.8, 80.8, 41.8],
-    [2,  30.9, 69.5, 32.4],
-    [3,  25.4,   57, 25.7],
-    [4,  11.7, 18.8, 10.5],
-    [5,  11.9, 17.6, 10.4],
-    [6,   8.8, 13.6,  7.7],
-    [7,   7.6, 12.3,  9.6],
-    [8,  12.3, 29.2, 10.6],
-    [9,  16.9, 42.9, 14.8],
-    [10, 12.8, 30.9, 11.6],
-    [11,  5.3,  7.9,  4.7],
-    [12,  6.6,  8.4,  5.2],
-    [13,  4.8,  6.3,  3.6],
-    [14,  4.8,  6.3,  3.6],
-    [15,  4.8,  6.3,  3.6],
-    [16,  4.8,  6.3,  3.6],
-    [17,  4.8,  6.3,  3.6],
-    [18,  4.8,  6.3,  3.6],
-    [19,  4.8,  6.3,  3.6],
-    [20,  4.8,  6.3,  3.6],
-    [21,  4.8,  6.3,  3.6],
-    [22,  4.8,  6.3,  3.6],
-    [23,  4.8,  6.3,  3.6]];
-    //do magic.h
+function load_data(){
+    var data_file = "TODO: NEED A LINK HERE";
+    var http_request = new XMLHttpRequest();
+    try{
+        // Opera 8.0+, Firefox, Chrome, Safari
+        http_request = new XMLHttpRequest();
+    }catch (e){
+        // Internet Explorer Browsers
+        try{
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+
+        }catch (e) {
+
+            try{
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            }catch (e){
+                // Something went wrong
+                alert("Your browser broke!");
+                return false;
+            }
+
+        }
+    }
+
+    http_request.onreadystatechange = function (){
+        if (http_request.readyState == 4){
+            // Javascript function JSON.parse to parse JSON data
+            var jsonObj = JSON.parse(http_request.responseText);
+            parse_data(jsonObj);
+        }
+    }
+    http_request.open("GET", data_file, true);
+    http_request.send();
+}
+
+function parse_data(jsonObj){
+    //TODO; DO MAGIC HERE
 
     draw_chart(data);
 }
 
 function draw_chart(data_array) {
-
     var data = new google.visualization.DataTable();
     data.addColumn('number', 'Time (hours)');
     data.addColumn('number', 'Weather');
@@ -55,5 +65,5 @@ function draw_chart(data_array) {
 
     chart.draw(data, options);
 
-    setTimeout(get_data, 300000); //5 minutes
+    setTimeout(load_data, 300000); //5 minutes
 }
