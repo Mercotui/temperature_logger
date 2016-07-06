6691666ea4082eeee8d787873201973cc5663f71
Menno van der Graaf <mennovandergraaf@hotmail.com>
2016-07-06 18:03:32 +0200

    add inital webserver database interface

--- web_viewer/web_server.c
+++ web_viewer/web_server.c
@@ -1,25 +1,32 @@
 //-----includes
+#include <sqlite3.h>
 #include <string.h>
+
 
 #include "../mongoose/mongoose.h"
 
 //-----defines
 #define DATA_PATH "/data.json"
 #define MAX_JSON_DATA_LEN 1000 // arbitrary lenght
+struct db_response {
+    int argc;
+    char** argv;
+    char** azColName;
+};
 
 //-----static variables
 static const char* s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
-char json_buffer[MAX_JSON_DATA_LEN];
 
 //-----static functions
 static void ev_handler (struct mg_connection* nc, int ev, void* p);
 static void serve_json_data (struct mg_connection* nc, struct http_message* hm);
-static void get_data ();
+static struct db_response* get_data (void);
 static size_t format_data ();
 static const char* json_cat_str (char* dest, const char* src);
 
 //-----function defenitions
+// http functions
 static void ev_handler (struct mg_connection* nc, int ev, void* p) {
     struct http_message* hm = (struct http_message*)p;
     if (ev == MG_EV_HTTP_REQUEST) {
@@ -35,9 +42,10 @@ static void ev_handler (struct mg_connection* nc, int ev, void* p) {
 
 static void serve_json_data (struct mg_connection* nc, struct http_message* hm) {
     size_t data_len;
+    char json_buff;
 
-    get_data ();
-    data_len = format_data ();
+    struct db_response* data = get_data ();
+    data_len                 = format_data (&json_buff, data);
 
     mg_printf (nc, "HTTP/1.1 200 OK\r\n"
                    "Cache: no-cache\r\n"
@@ -47,12 +55,31 @@ static void serve_json_data (struct mg_connection* nc, struct http_message* hm) 
     data_len, json_buffer);
 }
 
-static void get_data () {
-    ;
+// database functions
+static struct db_response get_data (char** buffer) {
+    char* err_msg = NULL;
+    int status;
+    sqlite3* db;
+
+    status = sqlite3_open ("temperatures.db", &db);
+    if (status != SQLITE_OK) {
+        fprintf (stderr, "Cannot open database: %s\n", sqlite3_errmsg (db));
+    } else {
+        status = sqlite3_exec (db,
+        "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;",
+        format_data, buffer, &err_msg);
+        if (status != SQLITE_OK) {
+            printf ("SQL error: %s\n", err_msg);
+            sqlite3_free (err_msg);
+        }
+    }
+
+    sqlite3_close (db);
+    return;
 }
 
-static size_t format_data () {
-
+// json functions
+static size_t format_data (char** json_buff, ) {
 
     return 5;
 }
