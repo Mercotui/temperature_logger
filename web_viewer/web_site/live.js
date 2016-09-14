ef5667d90385823067364b901e58920cbfc7dc14
Menno van der Graaf <mennovandergraaf@hotmail.com>
2016-09-14 22:25:49 +0200

    try to implement dynamic chart

--- web_viewer/web_site/live.js
+++ web_viewer/web_site/live.js
@@ -1,5 +1,60 @@
-google.charts.load('current', {'packages':['line']});
-google.charts.setOnLoadCallback(load_data);
+function onload(){
+    var chart = new CanvasJS.Chart("chart_div",{
+        zoomEnabled: true,
+        title: {
+            text: "Temperatures from datatbase"
+        },
+        toolTip: {
+            shared: true
+
+        },
+        legend: {
+            verticalAlign: "top",
+            horizontalAlign: "center",
+            fontSize: 14,
+            fontWeight: "bold",
+            fontFamily: "calibri",
+            fontColor: "dimGrey"
+        },
+        axisX: {
+            title: "chart updates every 3 secs"
+        },
+        axisY:{
+            prefix: '$',
+            includeZero: false
+        },
+        data: [{
+            // dataSeries1
+            type: "line",
+            xValueType: "dateTime",
+            showInLegend: true,
+            name: "Company A",
+            dataPoints: dataPoints1
+        },
+        {
+            // dataSeries2
+            type: "line",
+            xValueType: "dateTime",
+            showInLegend: true,
+            name: "Company B" ,
+            dataPoints: dataPoints2
+        }],
+        legend:{
+            cursor:"pointer",
+            itemclick : function(e) {
+                if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                }
+                else {
+                    e.dataSeries.visible = true;
+                }
+                chart.render();
+            }
+        }
+    });
+
+    load_data();
+}
 
 function load_data(){
     setTimeout(load_data, 10000);//300000); //5 minutes
@@ -39,31 +94,14 @@ function load_data(){
 }
 
 function parse_data(jsonObj){
-    //TODO; DO MAGIC HERE
+    dataPoints1.push({
+        x: time.getTime(),
+        y: yValue1
+    });
+    dataPoints2.push({
+        x: time.getTime(),
+        y: yValue2
+    });
 
-    draw_chart(data);
-}
-
-function draw_chart(data_array) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'Time (hours)');
-    data.addColumn('number', 'Weather');
-    data.addColumn('number', 'RaspberryTAU');
-    data.addColumn('number', 'Titus');
-
-    data.addRows(data_array);
-
-    var options = {
-        chart: {
-            title: 'Temperature',
-            subtitle: 'in degrees celsius'
-        },
-        //vAxis.minorGridlines.units:
-        width: 1500,
-        height: 500
-    };
-
-    var chart = new google.charts.Line(document.getElementById('chart_div'));
-
-    chart.draw(data, options);
+    chart.render();
 }
